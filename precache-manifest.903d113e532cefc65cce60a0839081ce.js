06c954906c8a348ac21d050d8f808b687917412c
Anindya Das <dasanindya9809@gmail.com>
2020-03-22 21:16:38 +0530

    Updates

---
+++ precache-manifest.903d113e532cefc65cce60a0839081ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c764aa1dd28fbadbb6964d7cc044ce0",
+    "url": "/deploy-react-app/index.html"
+  },
+  {
+    "revision": "c34daa658692e8c63a92",
+    "url": "/deploy-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1fe68d461dab9bdef04f",
+    "url": "/deploy-react-app/static/js/2.04cea63c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react-app/static/js/2.04cea63c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c34daa658692e8c63a92",
+    "url": "/deploy-react-app/static/js/main.2389ee01.chunk.js"
+  },
+  {
+    "revision": "95b55f1ad408ddae140d",
+    "url": "/deploy-react-app/static/js/runtime-main.66563c3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
